4117fd79646edbd951ea4bdcf5fe1ba91bcbf88c
bina100 <binaro97@gmail.com>
2023-06-21 14:10:27 +0300

    add test to songRepository

---
+++ addDependency/src/test/java/com/L100706addDependency/addDependency/song/SongRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.L100706addDependency.addDependency.song;
+
+import com.L100706addDependency.addDependency.singer.Singer;
+import com.L100706addDependency.addDependency.singer.SingerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class SongRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByLengthGreaterThan() {
+
+    }
+}
